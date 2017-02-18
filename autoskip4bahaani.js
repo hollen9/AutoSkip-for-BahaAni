01f2db05a04116b85d48ab72b31a554569f2893f
好冷酒 Hollen9 <hollen.tw@gmail.com>
2017-02-18 18:04:43 +0800

    設置 updateURL 為本 RawUrl

--- autoskip4bahaani.js
+++ autoskip4bahaani.js
@@ -5,7 +5,7 @@
 // @description  自動同意並跳過廣告──執行本腳本後，收看動畫瘋影片將無須手動點選「我同意」。並且，廣告在 30 秒後會自動跳過。
 // @author       hollen9
 // @match        http://ani.gamer.com.tw/animeVideo.php?*
-// @updateURL    
+// @updateURL    https://raw.githubusercontent.com/hollen9/AutoSkip-for-BahaAni/master/autoskip4bahaani.js
 // @grant        GM_updatingEnabled
 // ==/UserScript==
 
