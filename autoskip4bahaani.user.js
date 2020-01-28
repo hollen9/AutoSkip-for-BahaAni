34bc9119710ce44a16d5b1313e7fce36fdf5c93f
hollen9 <hollen.tw@gmail.com>
2020-01-28 20:16:29 +0800

    Update autoskip4bahaani.user.js

--- autoskip4bahaani.user.js
+++ autoskip4bahaani.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @name         自動同意並跳過廣告
-// @namespace    hollen9.bahamut.animation
+// @name         巴哈動畫瘋 - 全螢幕自動播放
+// @namespace    https://hollen9.github.io
 // @version      1.0.1
 // @description  自動同意並跳過廣告──執行本腳本後，收看動畫瘋影片將無須手動點選「我同意」。並且，廣告在 30 秒後會自動跳過。
-// @author       hollen9
+// @author       Hollen9
 // @match        https://ani.gamer.com.tw/animeVideo.php?*
 // @match        http://ani.gamer.com.tw/animeVideo.php?*
 // @updateURL    https://raw.githubusercontent.com/hollen9/AutoSkip-for-BahaAni/master/autoskip4bahaani.user.js
 // @grant        GM_updatingEnabled
 // ==/UserScript==
+
 
 (function() {
     'use strict';
